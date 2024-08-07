46d738915e1e33de57ca1fad08ec1d04c657d73a
mgfallen <minegoodfeeling@gmail.com>
2024-08-07 12:47:31 +0300

    init commit with initialazer

---
+++ src/test/java/holding_t_one/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package holding_t_one.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
